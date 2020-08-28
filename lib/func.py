8d955c5e7a5fe923c66e709fddad2bc6c3089529
E. Almqvist <h1g.server@gmail.com>
2020-08-28 19:32:35 +0200

    Added ai class and more func

--- lib/func.py
+++ lib/func.py
@@ -3,5 +3,8 @@ import numpy as np
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-def correctFunc(rgb):
-    return ( rgb[0], rgb[1], rgb[2] )
+def correctFunc(rgb): # generates the correct answer for the AI 
+    return ( rgb[2], rgb[1], rgb[0] ) # basically invert the rgb values
+
+def genRandomMatrix( x:int, y:int ):
+    return np.random.rand(x, y)
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+
+from lib.func import *
+
+class rgb(object):
+    def __init__(self):
+        self.weights = 1
+
+    def think(self, inputMatrix):
+        print("thonk: " + str( sigmoid(self.weights) ))
+
+def init(): # init func
+    bot = rgb()
+
+    bot.think(1)
+
+init()
